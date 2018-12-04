6cccac1154374da87fba736cd2324e39311b6dcf
gokul <gokul@22feettribalww.com>
2018-12-04 17:41:00 +0530

    starter init

---
+++ src/components/shared/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <v-app id="inspire">
+            <v-navigation-drawer v-model="drawer" fixed app>
+                <v-list dense>
+                    <v-list-tile>
+                        <v-list-tile-action>
+                            <v-icon>home</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Home</v-list-tile-title>
+                        </v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile>
+                        <v-list-tile-action>
+                            <v-icon>contact_mail</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>Contact</v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </v-list>
+                </v-navigation-drawer>
+            <v-toolbar class="light-blue" dark fixed app>
+                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+                <!-- <v-toolbar-title>Application</v-toolbar-title> -->
+                <v-spacer></v-spacer>
+                <v-toolbar-items right>
+                    <v-btn flat to="/">Home</v-btn>
+                    <v-btn flat to="/about">About</v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+        </v-app>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   router,

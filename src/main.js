b848e2cfc6cbd959a746acb7f50db3490e0cdb88
gokul <gokul@22feettribalww.com>
2018-12-05 18:18:14 +0530

    datatables

--- src/main.js
+++ src/main.js
@@ -3,15 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
 // index.js or main.js
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
-Vue.use(axios,VueAxios)
 
 new Vue({
   router,
